92addabb73d70b7fa8bc6ac6670ca2b613dc79bd
Sayra Flores <sayraflores@MacBook-Pro-2.local>
2022-04-15 16:05:11 -0400

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+prompt('Deposit Money OR Withdraw Money');
+var a = 'deposit money';
+var b = 'withdraw money';
+
+if(userInput = a);
+{console.log('How much would you like to deposit?')};
+
+else if(userInput = b)
+console.log;('How much would you like to withdraw?');
